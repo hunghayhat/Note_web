a51b978ba5476f471a3234101ffc61f8684d524a
Hung Hay Hat <diegocoshung19@gmail.com>
2024-04-28 11:22:08 +0700

    continue redux-example

--- redux-example/script.js
+++ redux-example/script.js
@@ -8,7 +8,7 @@ const initiateState = 0;
 
 function reducer(state = initiateState, action) {
     switch (action.type) {
-        case "DEPOSIT":
+        case 'DEPOSIT':
             return state + action.payload
         case "WITHDRAW":
             return state - action.payload
@@ -20,9 +20,43 @@ function reducer(state = initiateState, action) {
 
 // store
 
-const store = createStore(reducer)
+//actions
+
+function aciontDeposit(payload) {
+    return {
+        type: 'DEPOSIT',
+        payload
+    }
+}
+
+function actionWithdraw(payload) {
+    return {
+        type: "WITHDRAW",
+        payload
+    }
+}
+
+// DOM event
+
+const deposit = document.querySelector('#deposit')
+const withdraw = document.querySelector("#withdraw")
+
+
+//event handler
+
+deposit.onclick = function() {
+    store.dispatch(aciontDeposit(10))
+}
+
+withdraw.onclick = function () {
+    store.dispatch(actionWithdraw(10))
+}
+
+const store = window.store =  createStore(reducer)
+// bien window cho phep truy cap qua console
+
 function render () {
-    const output = document.querySelector("#output")
+    const output = document.querySelector('#output')
     output.innerText = store.getState()
 }
 
